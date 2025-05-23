b44159273a1694ca4183ec5bfe1a677acab1fc96
Syed Mohammad Anas Faaiz <syed.anasfaaiz@gmail.com>
2025-05-23 08:44:24 +0530

    Added static data for residents

--- client/src/app/resident/layout.jsx
+++ client/src/app/resident/layout.jsx
@@ -1,6 +1,18 @@
-import React from "react"
+"use client"
+
 import { ResidentSidebar } from "@/components/resident/sidebar"
 
 export default function ResidentLayout({ children }) {
-  return <ResidentSidebar>{children}</ResidentSidebar>
+  return (
+    <div className="min-h-screen">
+      <ResidentSidebar />
+      <div className="lg:pl-72">
+        <main className="py-10">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  )
 }
